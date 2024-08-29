34b527c3be65dfedd38fd2e7e5f7fab36f5fd07f
mishi233 <romeromichael9753@gmail.com>
2024-08-29 06:15:08 -0500

    Cambios

--- cliente rpc.py
+++ cliente rpc.py
@@ -1,19 +1,73 @@
 import xmlrpc.client
+import os
 
 # Conectarse al servidor XML-RPC
 server = xmlrpc.client.ServerProxy("http://127.0.0.1:12345/")
-nombre = input("Bienvenido, indique su nombre: ")
+
+#Manejo de terminal
+def clsTerminal():
+    # Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # Unix o Linux
+    else:
+        os.system('clear')
+
+#Inicio de usuario
+remitente = input("Bienvenido, indique su usuario: ")
+usuarios = server.usuariosRegistrados()
+if not (remitente in usuarios):
+    server.registrar(remitente)
+
+clsTerminal()
 
 opcion = 0
 while opcion != 3:
-    opcion = int(input("Indique una opcion: \n1 - Mandar Mensaje\n2 - Recibir Mensajes\n3 - Desconectar\n"))
+    opcion = int(input("Indique una opcion: \n1 - Mandar Mensaje\n2 - Ver Mensajes\n3 - Desconectar\n"))
 
     if opcion == 1:
-        mensaje = input("Indique el mensaje:")
-        server.mandarMensaje(nombre, mensaje)
+        #Mandar Mensaje
+        usuariosConRemitente = server.usuariosRegistrados()
+        usuarios = []
+        for user in usuariosConRemitente:
+            if user != remitente:
+                usuarios.append(user)
+        print("Estos son los usuarios activos:")
+        for usuario in usuarios:
+            print(usuario)
+        destinatario = input("¿A quien desea mandarle un mensaje?")
+        if (destinatario in usuarios):
+            mensaje = input("Indique el mensaje:")
+            server.mandarMensaje(remitente, destinatario, mensaje)
+            clsTerminal()
+            print("Mensaje enviado con éxito")
+        else:
+            clsTerminal()
+            print("El usuario no es valido")
+
     elif opcion == 2:
-        print(server.recibirMensajes())
+        #Ver mensajes
+        listaChats = server.conservacionesActivas(remitente)
+        if listaChats == 0:
+            clsTerminal()
+            print("No hay conversaciones")
+        else:
+            print("Conversaciones activas:")
+            for chat in listaChats:
+                print(chat)
+            chatSeleccionado = input("Seleccione con cual de los siguientes usuarios desea interactuar:")
+            if chatSeleccionado in listaChats:
+                clsTerminal()
+                print("Conversación: ")
+                mensajes = server.recibirMensajes(remitente, chatSeleccionado)
+                for mensaje in mensajes:
+                    print(mensaje[0], "dice: " , mensaje[1])
+                print()
+            else:
+                clsTerminal()
+                print("El usuario no se encuentra")
     elif opcion == 3:
         continue
     else:
+        clsTerminal()
         print("Opcion no válida ")
--- server rpc.py
+++ server rpc.py
@@ -1,22 +1,71 @@
 from xmlrpc.server import SimpleXMLRPCServer
 import threading
 
+usuarios = []
+
+class Mensajes:
+    def __init__(self, usuario1, usuario2):
+        self.usuario1 = usuario1
+        self.usuario2 = usuario2
+        self.mensajes = []
+
+    def registrarMensaje(self, remitente, mensaje):
+        self.mensajes.append([remitente, mensaje])
+
+    def clientesParticipantes(self, cliente):
+        if cliente == self.usuario1:
+            return self.usuario2
+        elif cliente == self.usuario2:
+            return self.usuario1
+        else:
+            []
+        
 mensajes = []
 
-def mandarMensaje(usuario, mensaje):
-    mensajes.append(f"{usuario}: {mensaje}")
-    return "Éxito"
+def registrar(usuario):
+    usuarios.append(usuario)
+    return "Registrado"
+    
+def usuariosRegistrados():
+    return usuarios
 
-def recibirMensajes():
-    print(mensajes)
-    return mensajes
+def mandarMensaje(remitente, destinatario, mensaje):
+    for mensajesListados in mensajes:
+        if mensajesListados.clientesParticipantes(remitente) != [] and mensajesListados.clientesParticipantes(destinatario) != []:
+            mensajesListados.registrarMensaje(remitente, mensaje)
+            return "Enviado"
+    nuevoMensaje = Mensajes(remitente, destinatario)
+    nuevoMensaje.registrarMensaje(remitente, mensaje)
+    mensajes.append(nuevoMensaje)
+    return "Enviado"
+
+def conservacionesActivas(remitente):
+    listaChatsActivos = []
+    for mensaje in mensajes:
+        participanteChat = mensaje.clientesParticipantes(remitente)
+        if participanteChat != []:
+            listaChatsActivos.append(participanteChat)
+    if listaChatsActivos == []:
+        return 0
+    else:
+        return listaChatsActivos
+    
+def recibirMensajes(remitente, destinatario):
+    for mensaje in mensajes:
+        if mensaje.clientesParticipantes(remitente) != [] and mensaje.clientesParticipantes(destinatario) != []:
+            return mensaje.mensajes
+        else: 
+            return None
 
 # Configurar el servidor XML-RPC
 server = SimpleXMLRPCServer(("127.0.0.1", 12345))
 print("Servidor XML-RPC escuchando en 127.0.0.1:12345")
 
 # Registrar las funciones para que estén disponibles remotamente
+server.register_function(registrar, "registrar")
+server.register_function(usuariosRegistrados, "usuariosRegistrados")
 server.register_function(mandarMensaje, "mandarMensaje")
+server.register_function(conservacionesActivas, "conservacionesActivas")
 server.register_function(recibirMensajes, "recibirMensajes")
 
 # Iniciar el servidor en un hilo separado
